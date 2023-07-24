8ebcb41c3b7c4e316dd6c06edb553b0987b9ca31
dianasul <sulkova.diana@gmail.com>
2023-07-24 20:48:22 +0200

    Added renderEmotionsRadios
    the emotionsArray is rendered in a list with the radio input type

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { catsData } from "/data.js";
 
+const emotionsRadios = document.querySelector("#emotion-radios");
+
 function getEmotionsArray(cats) {
   const emotionsArray = [];
   for (let cat of cats) {
@@ -11,4 +13,17 @@ function getEmotionsArray(cats) {
   return emotionsArray;
 }
 
-getEmotionsArray(catsData);
+function renderEmotionsRadios(cats) {
+  const emotionsList = getEmotionsArray(cats);
+  let emotionsHtml = "";
+
+  for (let emotion of emotionsList) {
+    emotionsHtml += `
+    <div class="radio">
+      <label for="${emotion}">${emotion}</label>
+      <input type="radio" name="radio" id="${emotion}" value="${emotion}" />
+   </div>`;
+  }
+  emotionsRadios.innerHTML = emotionsHtml;
+}
+renderEmotionsRadios(catsData);
