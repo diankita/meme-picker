9abb327b16e1f95586db89a10502c7b2f9f23bb8
dianasul <sulkova.diana@gmail.com>
2023-07-24 20:21:21 +0200

    Used catsData to get an array with emotions

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import { catsData } from "/data.js"
+import { catsData } from "/data.js";
 
-function getEmotionsArray(cats){
-    const emotionsArray = []
-    for (let i = 0; i < cats.length; i++){
-        for (let j=0; j < cats[i].emotionTags.length; j++){
-                emotionsArray.push(cats[i].emotionTags[j])
-        }
-    }
-console.log(emotionsArray)
+function getEmotionsArray(cats) {
+  const emotionsArray = [];
+  for (let cat of cats) {
+    for (let emotion of cat.emotionTags)
+      if (!emotionsArray.includes(emotion)) {
+        emotionsArray.push(emotion);
+      }
+  }
+  return emotionsArray;
 }
 
-getEmotionsArray(catsData)
-
+getEmotionsArray(catsData);
